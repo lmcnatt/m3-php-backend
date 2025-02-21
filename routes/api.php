048edaea83dc147c91b8a04c7cae4c2f79d240ac
Logan McNatt <lmcnatt@nex.io>
2025-02-21 15:47:11 -0700

    All the good changes

--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RegisterController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::controller(RegisterController::class)->group(function(): void{
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+    Route::post('logout', 'logout');
+});
