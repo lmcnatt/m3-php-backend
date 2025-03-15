fad00601dac9d050dc40e1c4e2e8062ef1e7a67d
Logan McNatt <lmcnatt@nex.io>
2025-03-15 16:47:18 -0600

    Removed Comments

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\API\UserController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::controller(RegisterController::class)->group(function(): void{
     Route::post('register', 'register');
     Route::post('login', 'login');
